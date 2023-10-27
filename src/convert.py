d2b739732fc3167390257b3d939ca73a2df6ca24
erastov-alex <erastov.alex@gmail.com>
2023-10-27 13:13:52 +0300

    upd gitignore, convert, settings, options

--- src/convert.py
+++ src/convert.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
 import supervisely as sly
 import os
 from dataset_tools.convert import unpack_if_archive
 import src.settings as s
 from urllib.parse import unquote, urlparse
 from supervisely.io.fs import get_file_name, get_file_size
+from supervisely.io.json import load_json_file
 import shutil
 
 from tqdm import tqdm
@@ -69,17 +71,150 @@ def count_files(path, extension):
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    dataset_path = os.path.join("TACO","data")
+    ann_path = os.path.join(dataset_path,"annotations.json")
+    batch_size = 30
+    ds_name = "ds"
+    errors = []
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
-
-    # ... some code here ...
-
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
-
-    # return project
+    # for r,d,f in os.walk(dataset_path): # RENAME FILES BECAUSE OF DUBLICATE
+    #     for file in f:
+    #         if file != "annotations.json":
+    #             old_path = os.path.join(r,file)
+    #             new_filename = f"{os.path.basename(r)}_{file}"
+    #             new_path = os.path.join(r,new_filename)
+    #             os.rename(old_path, new_path)
 
 
+    def recreate_name(name):
+        """
+        from
+        'batch_1\\batch_1_000006.jpg'
+        to
+        'batch_1/000012.jpg'
+        """
+        batch, badname = os.path.split(name)
+        if 'IMG' in badname:
+            print(1)
+        normalname = badname.split(batch)[-1][1:]
+        return f"{batch}/{normalname}"
+
+    def create_ann(image_path):
+        labels = []
+        tags = []
+
+        image_name = image_path.split(dataset_path)[-1][1:]
+        image_name = recreate_name(image_name)
+        img_height = image_name_to_shape[image_name][0]
+        img_wight = image_name_to_shape[image_name][1]
+
+        subfolder_value = image_path.split("\\")[-2]
+        subfolder = sly.Tag(subfolder_meta, value=subfolder_value)
+        tags.append(subfolder)
+
+        scene_data = image_name_to_scene.get(image_name)
+        if scene_data is not None:
+            for curr_scene_data in scene_data:
+                cat_value = idx_to_scene_categories.get(curr_scene_data)
+                if cat_value is not None:
+                    scene = sly.Tag(scene_meta, value=cat_value)
+                    tags.append(scene)
+
+        ann_data = image_name_to_ann_data[image_name]
+        for curr_ann_data in ann_data:
+            category_id = curr_ann_data[0]
+            supercategory_value = idx_to_supercategory[category_id]
+            supercategory = sly.Tag(supercategory_meta, value=supercategory_value)
+            obj_class = idx_to_obj_class[category_id]
+            polygons_coords = curr_ann_data[1]
+            for coords in polygons_coords:
+                exterior = []
+                for i in range(0, len(coords), 2):
+                    exterior.append([int(coords[i + 1]), int(coords[i])])
+                if len(exterior) < 3:
+                    errors.append(image_name)
+                    continue
+                poligon = sly.Polygon(exterior)
+                label_poly = sly.Label(poligon, obj_class, tags=[supercategory])
+                labels.append(label_poly)
+
+            bbox_coord = curr_ann_data[2]
+            rectangle = sly.Rectangle(
+                top=int(bbox_coord[1]),
+                left=int(bbox_coord[0]),
+                bottom=int(bbox_coord[1] + bbox_coord[3]),
+                right=int(bbox_coord[0] + bbox_coord[2]),
+            )
+            label_rectangle = sly.Label(rectangle, obj_class, tags=[supercategory])
+            labels.append(label_rectangle)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    supercategory_meta = sly.TagMeta("supercategory", sly.TagValueType.ANY_STRING)
+    scene_meta = sly.TagMeta("scene", sly.TagValueType.ANY_STRING)
+    subfolder_meta = sly.TagMeta("subfolder", sly.TagValueType.ANY_STRING)
+    meta = sly.ProjectMeta(tag_metas=[supercategory_meta, scene_meta, subfolder_meta])
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    idx_to_obj_class = {}
+    idx_to_supercategory = {}
+    image_id_to_name = {}
+    image_name_to_ann_data = defaultdict(list)
+    image_name_to_shape = {}
+    image_name_to_scene = {}
+    idx_to_scene_categories = {}
+
+    ann = load_json_file(ann_path)
+    for curr_category in ann["categories"]:
+        idx_to_supercategory[curr_category["id"]] = curr_category["supercategory"]
+        obj_class = sly.ObjClass(curr_category["name"], sly.AnyGeometry)
+        meta = meta.add_obj_class(obj_class)
+        idx_to_obj_class[curr_category["id"]] = obj_class
+    api.project.update_meta(project.id, meta.to_json())
+
+    for curr_image_info in ann["images"]:
+        image_id_to_name[curr_image_info["id"]] = curr_image_info["file_name"]
+        image_name_to_shape[curr_image_info["file_name"]] = (
+            curr_image_info["height"],
+            curr_image_info["width"],
+        )
+
+    for curr_scene_info in ann["scene_annotations"]:
+        image_id = curr_scene_info["image_id"]
+        image_name_to_scene[image_id_to_name[image_id]] = curr_scene_info["background_ids"]
+
+    for curr_ann_data in ann["annotations"]:
+        image_id = curr_ann_data["image_id"]
+        image_name_to_ann_data[image_id_to_name[image_id]].append(
+            [curr_ann_data["category_id"], curr_ann_data["segmentation"], curr_ann_data["bbox"]]
+        )
+
+    for curr_scene_cat in ann["scene_categories"]:
+        idx_to_scene_categories[curr_scene_cat["id"]] = curr_scene_cat["name"]
+
+    images_names = [im_name for im_name in image_name_to_shape.keys()]
+    images_names = [f"{im_name.split('/')[0]}_{im_name.split('/')[1]}" for im_name in images_names]
+
+    subfolder_dict = defaultdict()
+    for name in images_names:
+        value = f"{name.split('_')[0]}_{name.split('_')[1]}"
+        key = name
+        subfolder_dict[key]= value
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+    for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+        images_pathes_batch = [os.path.join(dataset_path,subfolder_dict[image_name], image_name) for image_name in img_names_batch]
+
+        img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns_batch)
+
+        progress.iters_done_report(len(img_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,37 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "TACO"
+PROJECT_NAME_FULL: str = "TACO: Trash Annotations in Context"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.CC_BY_4_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.WasteRecycling()]
+CATEGORY: Category = Category.General()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.InstanceSegmentation(),CVTask.SemanticSegmentation(),CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2019-06-10"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "http://tacodataset.org/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 7968663
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/taco"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://github.com/pedropro/TACO"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,19 +51,19 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = ["https://arxiv.org/abs/2003.06975"]
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"https://github.com/pedropro/TACO"}
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "https://github.com/pedropro/TACO"
+AUTHORS: Optional[List[str]] = ["Pedro F Proença", "Pedro Simões"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://pedropro.github.io/", "https://seomis.cc/"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {"__POSTTEXT__":"Also the dataset includes *subfolder* and *scene* image-level tags, *supercategory* object tag"}
 TAGS: Optional[List[str]] = None
 
 
